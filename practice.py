19ede28a885ca977830d59c06519c50b682a4512
IanBullard74 <ibullard@uwyo.edu>
2024-09-23 21:52:11 +0000

    Finished Lab.

---
+++ practice.py
@@ -0,0 +1,13 @@
+names = ["Ian", "Lee", "Gabriel","Trenton","Luke","Dom"]
+
+print(names)
+
+names.remove("Ian")
+
+print(names)
+
+for name in names:
+    print(f"{name} is at my table")
+
+names.sort()
+print(names)
